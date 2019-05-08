718e53e1ca0d5d3eacd425feb42d40f12e95f8b2
Gelsey Torres <gelsey.torres@gmail.com>
2019-05-08 10:57:33 -0400

    Adds exercises for 120

---
+++ exercises/120_OOP/oo_basics_classes_and_objects_1/what_are_you.rb
@@ -0,0 +1,18 @@
+# What Are You?
+#
+# Using the code from the previous exercise, add an #initialize method that prints I'm a cat! when a new Cat object is initialized.
+#
+# Code:
+
+class Cat
+  def initialize
+    puts "I'm a cat!"
+  end
+end
+
+kitty = Cat.new
+p kitty
+
+# Expected output:
+#
+# I'm a cat!
