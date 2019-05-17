fc842b058f06303fd2a18462d82705b2edd1829c
Gelsey Torres <gelsey.torres@gmail.com>
2019-05-17 12:24:39 -0400

    Updates player to use different frequencies vs random choices of equal weight

--- lessons/120_lessons/lesson_2/player.rb
+++ lessons/120_lessons/lesson_2/player.rb
@@ -48,15 +48,34 @@ class Human < Player
 end
 
 class Computer < Player
-  NAMES = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5']
+  # Let's have a hash - key is names and frequencies is an array of floats
+
+  NAMES = {
+    'R2D2' => [0.0, 1.0, 0.0, 0.0, 0.0],
+    'Hal' => [0.0, 0.5, 0.4, 0.1, 0.0],
+    'Chappie' => [0.1, 0.1, 0.1, 0.6, 0.1],
+    'Sonny' => [0.3, 0.3, 0.2, 0.1, 0.2],
+    'Number 5' => [0.4, 0.3, 0.2, 0.1]
+  }
 
   def set_name
-    self.name = NAMES.sample
+    self.name = NAMES.keys.sample
   end
 
   def choose
-    # self.move = Move.new(Move::VALUES.sample)
-    choice = Move::VALUES.sample
+    # Then in here we can do this
+    # Create a hash with each move as a key, each frequency as a value
+    # Then we can loop over each frequency, and then we can perform rand**(1.0/weight)
+    # Return first element
+
+    frequencies = NAMES[name]
+
+    moves_and_freqs = Move::VALUES.zip(frequencies).to_h
+
+    choice = moves_and_freqs.max_by do |_, weight|
+      rand**(1.0/weight)
+    end.first
+
     self.move =
     Object.const_get(choice.capitalize).new(choice)
   end
